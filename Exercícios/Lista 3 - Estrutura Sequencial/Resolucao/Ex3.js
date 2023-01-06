7057814990da2d18936af25dd4b0cdedcd718985
Viniart <vinicioart4@hotmail.com>
2023-01-06 17:21:26 -0300

    Mais resoluções adicionadas

---
+++ Exercícios/Lista 3 - Estrutura Sequencial/Resolucao/Ex3.js
@@ -0,0 +1,14 @@
+// Tempo de duração de um evento
+var tempoSegundos = 3600;
+
+console.log(tempoSegundos);
+
+var tempoMinutos = tempoSegundos / 60;
+console.log(tempoMinutos);
+
+var tempoHoras = tempoMinutos / 60;
+console.log(tempoHoras);
+
+
+
+
---
+++ Exercícios/Lista 3 - Estrutura Sequencial/Resolucao/Ex8.js
@@ -0,0 +1,10 @@
+var custoDeFabrica = 3000;
+
+// Custo do distribuidor = 30% do custo de fabrica
+var custoDistribuidor = custoDeFabrica * 0.3;
+
+// Custo dos impostos = 45% do custo de fabrica
+var custoImpostos = custoDeFabrica * 0.45;
+
+// Custo do consumidor = soma de todos os custos
+var custoConsumidor = custoDeFabrica + custoDistribuidor + custoImpostos;
